dead7525e43ea8af394d2433860a57bb8e3d6b5f
Nicolas Hallaert <nicolas.hallaert.pro@gmail.com>
2021-02-20 18:35:20 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,97 @@
+# nHTMLToPDF
+Node lib for converts HTML or url to PDF file
+
+## Installation
+- Coming soon
+
+## Usage
+
+```js
+async function main() {
+  try {
+    const browser = await pdf.initBrowser();
+    const result1 = await pdf.generatePDF(browser, files);
+    const stream = await fs.createWriteStream(`./${your_pdf_name}.pdf`);
+    await stream.write(result1[0].buffer);
+    await pdf.terminateBrowser
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+main().catch(console.error);
+```
+
+### initBrowser(): Promise<Browser>
+
+```js
+async function main() {
+  try {
+    const browser = await pdf.initProcess();
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+main().catch(console.error);
+```
+
+### generatePDF(browser: Browser, files: pdfFile[], options?: PDFOPtions): Promise<PDF[]>
+
+```ts
+interface pdfFile {
+  content?: string,
+  url?: string,
+  options?: {}
+}
+
+interface PDFOptions {
+  path?: string,
+  scale?: number,
+  displayHeaderFooter?: boolean,
+  headerTemplate?: string,
+  footerTemplate?: string,
+  printBackground?: boolean,
+  landscape?: boolean,
+  pageRanges?: string,
+  format?: string,
+  width?: string | number,
+  height?: string | number,
+  margin?: {
+    top?: string | number,
+    right?: string | number,
+    bottom?: string | number,
+    left?: string | number,
+  },
+  preferCSSPageSize?: boolean
+}
+
+interface PDF {
+  options?: string,
+  buffer: Buffer
+}
+```
+
+```js
+async function main() {
+  try {
+    const browser = await pdf.initBrowser();
+    const result1 = await pdf.generatePDF(browser, files);
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+main().catch(console.error);
+```
+
+### terminateBrowser(browser: Browser): Promise<void>
+
+```js
+async function main() {
+  try {
+    const browser = await pdf.initBrowser();
+    await pdf.terminateBrowser
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+main().catch(console.error);
+```
---
+++ src/index.ts
@@ -0,0 +1,111 @@
+import { Browser } from "puppeteer";
+import * as puppeteer from "puppeteer";
+const compile = require('zup');
+
+module.exports
+/**
+ * @author HALLAERT Nicolas
+ * @returns {Promise<Browser>}
+ */
+async function initBrowser(): Promise<Browser> {
+  try {
+    return await puppeteer.launch();
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+interface pdfFile {
+  content?: string,
+  url?: string,
+  options?: {}
+}
+
+interface PDFOptions {
+  path?: string,
+  scale?: number,
+  displayHeaderFooter?: boolean,
+  headerTemplate?: string,
+  footerTemplate?: string,
+  printBackground?: boolean,
+  landscape?: boolean,
+  pageRanges?: string,
+  format?: string,
+  width?: string | number,
+  height?: string | number,
+  margin?: {
+    top?: string | number,
+    right?: string | number,
+    bottom?: string | number,
+    left?: string | number,
+  },
+  preferCSSPageSize?: boolean
+}
+
+interface PDF {
+  options?: string,
+  buffer: Buffer
+}
+
+/**
+ * @author HALLAERT Nicolas
+ * @description Return a buffer of generated pdfs with the payload of options used
+ *
+ * @param {Browser} browser
+ * @param {pdfFile[]} files
+ * @param {PDFOptions} [options]
+ * @returns {Promise<PDF[]>}
+ */
+async function generatePDF(browser: Browser, files: pdfFile[], options?: PDFOptions): Promise<PDF[]> {
+  const pdfs = [];
+
+  try {
+    const page = await browser.newPage();
+
+    for (let file of files) {
+      if (file.content) {
+        const template = compile(file.content);
+        const html = template(file?.options ?? {});
+
+        await page.setContent(html, {
+            waitUntil: 'networkidle0'
+        });
+      } else {
+        await page.goto(file.url!, {
+            waitUntil: 'networkidle0'
+        });
+      }
+
+      const pdf = JSON.parse(JSON.stringify(file));
+      delete pdf['content'];
+
+      pdf['options'] = options ?? undefined;
+      pdf['buffer'] = await page.pdf(pdf.options);
+      pdfs.push(pdf);
+    }
+  } catch (error) {
+    throw new Error(error);
+  } finally {
+    return pdfs;
+  }
+}
+
+/**
+ * @author HALLAERT Nicolas
+ *
+ * @param {Browser} browser
+ * @returns {Promise<void>}
+ */
+async function terminateBrowser(browser: Browser): Promise<void> {
+  try {
+    await browser.close();
+  } catch (error) {
+    throw new Error(error);
+  }
+
+  return;
+}
+
+module.exports.generatePDF = generatePDF;
+module.exports.initProcess = initBrowser;
+module.exports.terminateProcess = terminateBrowser;
